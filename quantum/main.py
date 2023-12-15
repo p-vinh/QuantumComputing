d00c9b841e917cae2fafd1def5a85f7691448270
random-debug <85326677+p-vinh@users.noreply.github.com>
2023-12-14 19:00:13 -0800

    Updated gitignore

--- quantum/main.py
+++ quantum/main.py
@@ -1,12 +1,18 @@
-from qiskit_ibm_runtime import QiskitRuntimeService
+# %%
+from qiskit_ibm_runtime import QiskitRuntimeService, Sampler
+from qiskit import QuantumCircuit
 import os
 from os.path import join, dirname
-import dotenv
+from dotenv import load_dotenv
 
-dotenv.load_dotenv("quantum\tokens.env")
 
-# Save an IBM Quantum account and set it as your default account.
-# QiskitRuntimeService.save_account(channel="ibm_quantum", token=os.environ.get("TOKEN"), set_as_default=True)
- 
-# # Load saved credentials
-# service = QiskitRuntimeService()
+
+circuit = QuantumCircuit(2, 2)
+
+circuit.x(0)
+circuit.h(0)
+circuit.barrier()
+
+circuit.measure(range(2), range(2))
+circuit.draw(output='mpl', style="iqp")
+
